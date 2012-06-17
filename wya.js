4d6f929f5d4cebcf6a25e600f5bb07f3faa4213e
Ricardo Havill <t.ricardo.havill@morris.com>
2012-06-17 13:36:05 -0400

    cleaning up code.

--- wya.js
+++ wya.js
@@ -1,59 +1,38 @@
 // Listen for any attempts to call changePage().
-$(document).bind( "pagebeforechange", function( e, data ) {
-	// We only want to handle changePage() calls where the caller is
-	// asking us to load a page by URL.
-	if ( typeof data.toPage === "string" ) {
-		var u = $.mobile.path.parseUrl( data.toPage );
-		if (u.hash == '#my-friends') {
-			loadFriends();
-                }
-		else if (u.hash == '#login') {
-			alert('login page')
-		}
-//			e.preventDefault();
-	}
+$(document).bind( "pagebeforechange", function( e, data ) { 
+  if ( typeof data.toPage === "string" ) {
+    var u = $.mobile.path.parseUrl( data.toPage );
+    if (u.hash == '#my-friends') {
+      loadFriends();
+    }
+    else if (u.hash == '#login') {
+      alert('login page')
+    }
+    //e.preventDefault();
+  }
 });
-$(document).ready(function () {
-  loadFriends();
-});
-function loadFriends() {
-  alert('get friends..');
-  callJSONP('http://wya/rest/user-relationships.jsonp?callback=whatNow');
-/*
-  var req = $.ajax({
-    url: 'http://wya/rest/user-relationships',
-    dataType: 'xml'
-    //data: data,
-    success: function (data, textStatus, jqXHR) {
-	alert('success');
-    } ,
-    error: function (jqXHR, textStatus, errorThrown) {
- 	alert('error');
-    } 
-  });
-  req.done(function(msg) {
-    alert( msg );
-  });
 
-  req.fail(function(jqXHR, textStatus) {
-    alert( "Request failed: " + textStatus );
+$(document).ready(function () {
+  if (location.hash == '' || location.hash == '#my-friends') {
+    loadFriends();
+  }
+});
+
+function listFriends(friends) {
+  $.each(friends, function(key, value) { 
+  alert(value.rid); 
   });
-*/
 }
 
-function listFriends(a,b) {
-  alert('list friends');
+function loadFriends() {
+  alert('geting friends..');
+  callJSONP('http://wya/rest/user-relationships.jsonp?callback=listFriends');
 }
 
 function callJSONP(url) {
   if (url) {
     var script = document.createElement('script');
     script.src = url;
-     
-    // append the script in the document body. 
-    // As per on-deman script behaviour as soon as you add the script to 
-    // the document,the script will be execute and the web service will 
-    // be called.
     document.body.appendChild(script); 
   }
 }
